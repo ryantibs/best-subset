a4fa0ea8b82efd535638f070c4920cc4da3e0b87
Ryan Tibshirani <ryantibs@gmail.com>
2018-08-30 22:54:30 -0400

    Manually overwrites coef and predict functions for L0Learn

---
+++ sims/L0LearnNew.R
@@ -0,0 +1,25 @@
+# New coef and predict functions for L0Learn. Just allow them to take no lambda
+# or gamma arguments, and return coefficients/predictions over all tunings
+coef.L0LearnNew = function(object) {
+  class(object) = "L0Learn"
+  mat = coef(object,lambda=object$lambda[[1]],gamma=object$gamma[1])
+  if (length(object$gamma) > 1) {
+    for (i in 2:length(object$gamma)) {
+      mat = cbind(mat, coef(object, lambda=object$lambda[[i]],
+                            gamma=object$gamma[i]))
+    }
+  }
+  return(mat)
+}
+
+predict.L0LearnNew = function(object) {
+  class(object) = "L0Learn"
+  mat = predict(object,lambda=object$lambda[[1]],gamma=object$gamma[1])
+  if (length(object$gamma) > 1) {
+    for (i in 2:length(object$gamma)) {
+      mat = cbind(mat, predict(object, lambda=object$lambda[[i]],
+                               gamma=object$gamma[i]))
+    }
+  }
+  return(mat)
+}
