796e81713634aef8b66363f9740efed5df3ba213
Trevor Hastie <hastie@stanford.edu>
2023-07-24 15:32:33 -0700

    Update fig.med.R
    Had the wrong file. Now correct

--- sims/fig.med.R
+++ sims/fig.med.R
@@ -1,27 +1,27 @@
 ## plots for high-dimensional simulation, s=5
 library(bestsubset)
-n = 50; p = 1000
-file.list = system(paste0("ls ","rds/hi5/*.rds"),intern=TRUE)
+n = 500; p = 100
+file.list = system(paste0("ls ","rds/med/*.rds"),intern=TRUE)
 method.nums = c(7,2,1,3)
 method.names = c("Best subset","Forward stepwise","Lasso","Relaxed lasso")
 
 
 plot.from.file(file.list, what="error", rel.to=NULL, tuning="val",
                method.nums=method.nums, method.names=method.names,
-               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/hi5",
-               file.name="hi5.err")
+               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/med",
+               file.name="med.err")
 
 plot.from.file(file.list, what="prop", rel.to=NULL, tuning="val",
                method.nums=method.nums, method.names=method.names,
-               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/hi5",
-               file.name="hi5.prop")
+               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/med",
+               file.name="med.prop")
 
 plot.from.file(file.list, what="nonzero", rel.to=NULL, tuning="val",
                method.nums=method.nums, method.names=method.names,
-               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/hi5",
-               file.name="hi5.nzs")
+               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/med",
+               file.name="med.nzs")g
 
 plot.from.file(file.list, what="F", rel.to=NULL, tuning="val",
                method.nums=method.nums, method.names=method.names,
-               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/hi5",
-               file.name="hi5.F")
+               legend.pos="bottom", make.pdf=TRUE, fig.dir="fig/med",
+               file.name="med.F")
